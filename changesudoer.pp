4f470807e698f2d1b198b9623498c6a0aebb3f23
Flame <s3464796@student.rmit.edu.au>
2017-10-12 19:01:43 +1100

    change sudoer + linter check

---
+++ changesudoer.pp
@@ -0,0 +1,14 @@
+#change sudoers file
+class changesudoer {
+augeas { "sudoers":
+context => "/etc/sudoers",
+changes => [
+"set spec[user = 'becca']/user becca",
+"set spec[user = 'becca']/host_group/host ALL",
+"set spec[user = 'becca']/host_group/command ALL",
+"set spec[user = 'becca']/host_group/command/runas_user ALL",
+],
+}
+
+
+}
