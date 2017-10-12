89399a074cfb58d87fe18b0e225bac3bc6485c80
Flame <s3464796@student.rmit.edu.au>
2017-10-12 19:25:07 +1100

    bug fix part 2

--- changepath.pp
+++ changepath.pp
@@ -1,6 +1,6 @@
 #add path to global.
 class changepath{
 exec { 'sudo export PATH=$PATH:/usr/local/bin' :
-path => ['/usr/bin','/usr/sbin','/usr/local/bin'],
+path => ['/usr/bin','/usr/sbin','/usr/local/bin','/user/local/sbin','/bin','/sbin','],
 }
 }
