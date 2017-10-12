78b0511ea5de0aaa1b8e9cf845dc75f50ef18870
Flame <s3464796@student.rmit.edu.au>
2017-10-12 19:11:08 +1100

    apache config class

---
+++ apache.pp
@@ -0,0 +1,20 @@
+#apache server setup
+class apache (String $version = 'installed') {
+package {'httpd':
+ensure => $version,
+}
+#run apache server
+#service {'httpd':
+#ensure => running,
+#}
+#create root file 
+file{"/var/www/s3464796":
+ensure => 'directory',
+mode   => '0755',
+}
+#change document root 
+augeas {"httpd_conf" :
+context =>"/files/etc/httpd/conf",
+changes =>'set DocumentRoot "/var/www/s3464796"',
+}
+}
