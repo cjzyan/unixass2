12f9789cc6aa4e68a4e45c9d18254e564158a9b6
Flame <s3464796@student.rmit.edu.au>
2017-10-13 17:46:24 +1100

    add vncserver

--- repository.pp
+++ repository.pp
@@ -44,4 +44,8 @@ ensure => present,
 exec { 'wget https://jaist.dl.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm ; sudo rpm -Uvh dia2code-0.8.3-3.1.i586.rpm ; sudo rm dia2code-0.8.3-3.1.i586.rpm' :
 path   => ['/usr/bin','/usr/sbin','/usr/local/bin'],
 }
+
+package { 'tigervnc-server' :
+ensure => present,
+}
 }
