648aba13b5b79fc5f07d6de24ed91c62f1cd478e
Flame <s3464796@student.rmit.edu.au>
2017-10-13 20:10:39 +1100

    Host fix

--- hosts.pp
+++ hosts.pp
@@ -1,11 +1,8 @@
 #This class is for editing hosts file for short titan.
 class hosts {
-augeas { "hosts":
-context => "/etc/hosts",
-changes => [
-"set *[ipaddr = '131.170.5.131']/canonical titan",
-"set *[ipaddr = '131.170.5.131']/alias[1] titan.csit.rmit.edu.au",
-],
+host { 'titan.csit.rmit.edu.au':
+ip           => '131.170.5.131',
+host_aliases => 'titan',
 }
 }
 
